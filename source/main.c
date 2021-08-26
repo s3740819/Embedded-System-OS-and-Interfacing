848543b0b6765ae16205ccb34e0249b25606979b
s3740819 <s3740819@rmit.edu.vn>
2021-08-27 02:49:18 +0700

    push

--- source/main.c
+++ source/main.c
@@ -4,8 +4,35 @@ void main(){
 	uart_init();
 	framebf_init();
 	display_background(); 
-
+	display_spider(280,110, 0);
+	display_spider(490,110, 1);
+	display_spider(700,110, 2);
+	select(0);
+	int animal = 0;
 	while(1) {
+		char c = uart_getc();
+		if (c == 'a' || c == 'A'){
+			if (animal > 0) {
+				display_background(); 
+				display_spider(280,110, 0);
+				display_spider(490,110, 1);
+				display_spider(700,110, 2);
+				animal--;
+				select(animal);
+			}
+		}
+		else if (c == 'd' || c== 'D'){
+			if (animal < 2) {
+				display_background(); 
+				display_spider(280,110, 0);
+				display_spider(490,110, 1);
+				display_spider(700,110, 2);
+				animal ++;
+				select(animal);
+			}
+		}
+		else if (c == '\n') uart_puts("Play!");
+		else if (c == 27) uart_puts("OUT!");
 	}
 }
 
--- source/uart.c
+++ source/uart.c
@@ -55,16 +55,14 @@ void uart_sendc(unsigned char c) {
 char uart_getc() {
     char c;
 
-    /* wait until data is ready (one symbol) */
-    do {
-    	asm volatile("nop");
-    } while ( !(*AUX_MU_LSR & 0x01) );
-
-    /* read it and return */
-    c = (char)(*AUX_MU_IO);
-
-    /* convert carriage return to newline */
-    return (c == '\r' ? '\n' : c);
+    if ((*AUX_MU_LSR & 0x01)){
+		/* read it and return */
+		c = (char)(*AUX_MU_IO);
+	
+		/* convert carriage return to newline */
+		return (c == '\r' ? '\n' : c);
+    }
+    return 0;
 }
 
 /**
