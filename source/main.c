1c3062a268b0638bf2288f30091fcc5dc563ce00
tvppaul <46493098+tvppaul@users.noreply.github.com>
2021-08-30 18:49:19 +0700

    Add files via upload

--- source/main.c
+++ source/main.c
@@ -7,7 +7,7 @@
 void main(){
 	uart_init();
 	framebf_init();
-
+	//drawRectangleARGB32()
 	while(1) {
 		char c =  uart_getc();
 		if (c == 'g'){
@@ -15,6 +15,11 @@ void main(){
 		}
 		else if (c == 'v'){
 			execute_video();
+		}
+		else if(c == 'f') {
+			drawString("Nguyen Le - s3777242", 100, 100, 0x00FF0000);
+			drawString("Bao Tran - s3740819", 100, 116, 0x0000FF00);
+			drawString("Phuc To - s3758272", 100, 132, 0x00FFFFFF);
 		}
 	}
 }
